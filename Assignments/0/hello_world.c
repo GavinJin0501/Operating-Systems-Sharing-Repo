7182dc5dbebd64b75182cd6b8ea01d70d14b24e2
Gavin Jin <jj2915@nyu.edu>
2022-09-15 09:29:10 +0800

    remove python files

--- Assignments/0/hello_world.c
+++ Assignments/0/hello_world.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 
-void swap1(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-
 int main() {
-	int a = 0;
-	int b = 1;
-	printf("a: %d, b: %d\n", a, b);
-	printf("a address: %p, b address: %p\n", &a, &b);
-
-	swap1(&a, &b);
-	printf("a: %d, b: %d\n", a, b);
-	printf("a address: %p, b address: %p\n", &a, &b);
-
-
-
+	printf("Hello world!\n");
 	return 0;
 }
