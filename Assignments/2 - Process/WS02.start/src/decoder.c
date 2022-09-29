aa71c6e8c551324d1edea11671817fac3f5e4ed5
Gavin Jin <jj2915@nyu.edu>
2022-09-29 17:07:54 +0800

    Assignment2 Q2 complete

--- Assignments/2 - Process/WS02.start/src/decoder.c
+++ Assignments/2 - Process/WS02.start/src/decoder.c
@@ -11,6 +11,7 @@
 
 
 #define _XOPEN_SOURCE 700
+#define NUM_OF_CHILDREN 10
 
 
 int magicsq[3][3] = {{4, -1, 8}, {-1, -1, -1}, {2, -1, 6}};
@@ -50,11 +51,48 @@ void display() {
 }
 
 
-int solve(int x) {
+int solve(int a) {
 	/* CODE THAT EVERY CHILD PROCESS EXECUTES */
+	int sum, b, c, d, e;
+	
+	magicsq[0][1] = a;
+	sum = magicsq[0][0] + magicsq[0][1] + magicsq[0][2];
+	b = sum - magicsq[0][0] - magicsq[2][0];
+	d = sum - magicsq[0][2] - magicsq[2][2];
+	e = sum - magicsq[2][0] - magicsq[2][2];
+	c = sum - a - e;
+
+	magicsq[1][0] = b;
+	magicsq[1][1] = c;
+	magicsq[1][2] = d;
+	magicsq[2][1] = e;
+
+	if (check() == 1) {
+		display();
+		return 1;
+	}
+	return 0;
 }
 
 int main(int argc, char **argv)
 {	
 	/* PARENT PROCESS */
+	int i, pid;
+
+	for (i = 0; (i < NUM_OF_CHILDREN) && ((pid = fork()) > 0); i++);
+
+	if (pid == -1) {
+		perror("fork");
+		exit(-1);
+	} else if (pid == 0) {
+		int val = solve(i);
+		if (val == 1) {
+			printf("Child %d> my pid is: %d\n", i, getpid());
+		}
+		exit(0);
+	} else {
+		for (int j = 0; i < NUM_OF_CHILDREN; j++) {
+			wait(0);
+		}
+	}
 }
