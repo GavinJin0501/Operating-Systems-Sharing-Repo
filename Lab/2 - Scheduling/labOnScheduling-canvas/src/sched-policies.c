cca81d9536882cb5a5702eb56e8ecd57ce25586f
Gavin Jin <jj2915@nyu.edu>
2022-10-13 14:38:49 +0800

    lab 2 question 3 finished

--- Lab/2 - Scheduling/labOnScheduling-canvas/src/sched-policies.c
+++ Lab/2 - Scheduling/labOnScheduling-canvas/src/sched-policies.c
@@ -16,23 +16,23 @@ int admitNewTasks(task tasks[], int nbOfTasks, sched_data* schedData, int curren
     return 1;
 }
 
-// return the index in tasks[] of the shortest job
-int getShortestJobIndex(task tasks[], sched_data* schedData, int* iOfQueue) {
+// return the index in tasks[] of the shortest job (total time/remaining time)
+// mode: 1 - SJF, 2 - SRTF
+int getShortestJobIndex(task tasks[], sched_data* schedData, int mode) {
     if (schedData->queues[0][0] == -1) {
         return -1;
     }
 
     int i = 0;                                      // index to traverse schedData->queues
     int ans = schedData->queues[0][i];              // store the answer
-    int shortest_time = tasks[ans].computationTime;
+    int shortest_time = (mode == 1) ? tasks[ans].computationTime : tasks[ans].computationTime - tasks[ans].executionTime;
     int temp_ans;                                   // intermediate ans
-    *iOfQueue = i;
     
     while ((temp_ans = schedData->queues[0][i]) != -1) {
-        if (tasks[temp_ans].computationTime < shortest_time) {
+        int temp_time = (mode == 1) ? tasks[temp_ans].computationTime : tasks[temp_ans].computationTime - tasks[temp_ans].executionTime;
+        if (temp_time  < shortest_time) {
             ans = temp_ans;
-            shortest_time = tasks[temp_ans].computationTime;
-            *iOfQueue = i;
+            shortest_time = temp_time;
         }
         i++;
     }
@@ -110,35 +110,33 @@ int SJF(task tasks[], int nbOfTasks, sched_data* schedData, int currentTime) {
     //      i is the index in the task
     //      j is the index in the job queue
     j = 0;
-    while ((i = schedData->queues[0][j]) != -1) {
-        /* if it is running, check if it has finished */ 
-        if (tasks[i].state == RUNNING) {
-            /* f finished, terminate it and remove it from the queue */
-            if (tasks[i].executionTime == tasks[i].computationTime) {
-                tasks[i].state = TERMINATED;
-                tasks[i].completionDate = currentTime;
-                for (; j < MAX_NB_OF_TASKS - 1; j++) {
-                    schedData->queues[0][j] = schedData->queues[0][j+1];
-                }
-            } else {
-                /* if not, reelect this task */
-                tasks[i].executionTime ++;
-                return i;
+    while ((i = schedData->queues[0][j]) != -1 && tasks[i].state != RUNNING) j++;
+    printf("%d, %d\n", i, j);
+
+    /* i is the index of the task that is running if i is not -1 */ 
+    if (i != -1) {
+        // if finished, terminate it, remove it from the queue, and elect a new task
+        if (tasks[i].executionTime == tasks[i].computationTime) {
+            tasks[i].state = TERMINATED;
+            tasks[i].completionDate = currentTime;
+            for (; j < MAX_NB_OF_TASKS - 1; j++) {
+                schedData->queues[0][j] = schedData->queues[0][j+1];
             }
-            break;
         } else {
-            j++;
+            /* if not, reelect this task */
+            tasks[i].executionTime ++;
+            return i;
         }
     }
 
-    // Otherwise, elect the shortest job
-    i = getShortestJobIndex(tasks, schedData, &j);
+    // Otherwise, try to elect a new task if no task 
+    i = getShortestJobIndex(tasks, schedData, 1);
     if (i != -1){
         tasks[i].executionTime ++;
         tasks[i].state = RUNNING;
         return i;
     }
-
+    
     // No task could be elected
     return -1;
 }
@@ -146,6 +144,53 @@ int SJF(task tasks[], int nbOfTasks, sched_data* schedData, int currentTime) {
 
 int SRTF(task tasks[], int nbOfTasks, sched_data* schedData, int currentTime) {
     //TODO (LAB Q3)
+    int i, j, sr_job;
+
+    // Initialize single queue
+    if (currentTime == 0) {
+        printf("Initializing job queue\n");
+        schedData->nbOfQueues = 1;
+        for (i = 0; i < MAX_NB_OF_TASKS; i++) {
+            schedData->queues[0][i] = -1;
+        }
+    }
+
+    admitNewTasks(tasks, nbOfTasks, schedData, currentTime);
+    printQueues(tasks, schedData);
+
+    // find if there is a running task
+    sr_job = getShortestJobIndex(tasks, schedData, 2);
+    j = 0;
+    while ((i = schedData->queues[0][j]) != -1 && tasks[i].state != RUNNING) j++;
+    printf("sr_job index: %d, runnning_job index: %d\n", sr_job, i);
+    printf("Task %d: executionTime: %d, computationTime: %d\n", i + 1, tasks[i].executionTime, tasks[i].computationTime);
+
+    // if there is a running task with index i
+    if (i != -1) { 
+        if (tasks[i].executionTime == tasks[i].computationTime) { // if running task has finished
+            tasks[i].state = TERMINATED;
+            tasks[i].completionDate = currentTime;
+            for (; j < MAX_NB_OF_TASKS - 1; j++) {
+                schedData->queues[0][j] = schedData->queues[0][j+1];
+            }
+        } else if (i != sr_job) { // if running task does not have the shortest remaining time
+            tasks[i].state = SLEEPING;
+            tasks[sr_job].executionTime ++;
+            tasks[sr_job].state = RUNNING;
+            return sr_job;
+        } else {
+            tasks[i].executionTime ++;
+            return i;
+        }
+    } 
+
+    sr_job = getShortestJobIndex(tasks, schedData, 2);
+    if (sr_job != -1) {
+        tasks[sr_job].executionTime ++;
+        tasks[sr_job].state = RUNNING;
+        return sr_job;
+    }
+
     return -1;
 }
 
