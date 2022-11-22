5943ced342af7a1f36f94531372712371ed22ebc
Gavin Jin <jj2915@nyu.edu>
2022-11-22 23:08:05 +0800

    update

--- Lab/4 - Threads/src/exercise4.c
+++ Lab/4 - Threads/src/exercise4.c
@@ -13,15 +13,25 @@
 
 
 /* TODO: global variables */
+pthread_mutex_t mx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int count;
 
 
 void wait_barrier(int n) {
     /* TODO: synchronization */
+    pthread_mutex_lock(&mx);
+    count++;
+    if (count < n)
+        pthread_cond_wait(&cond, &mx);
+    else
+        pthread_cond_broadcast(&cond);
+    pthread_mutex_unlock(&mx);
 }
 
 
 void* thread_func(void* arg) {
-    printf ("afore barrier\n");
+    printf ("before barrier\n");
     wait_barrier (NB_THREADS);
     printf ("after barrier\n");
     pthread_exit ( (void*)0);
@@ -34,6 +44,7 @@ int main(int argc, char** argv) {
     pthread_t tid[NB_THREADS];
     
     /* initialize global variables */
+    count = 0;
     
     for (i = 0; i < NB_THREADS; i++)
         pthread_create((tid+i), 0, thread_func, 0);
--- Lab/4 - Threads/src/exercise5.c
+++ Lab/4 - Threads/src/exercise5.c
@@ -9,13 +9,39 @@
 #include <ctype.h>
 #include <sched.h>
 
-#define NB_THREADS 4
-#define NB_LOOPS 5
+#define NB_THREADS 3
+#define NB_LOOPS 2
 
 /* TODO: SYNCHRONIZATION VARIABLES */
+pthread_mutex_t mx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condIn = PTHREAD_COND_INITIALIZER;
+pthread_cond_t condOut = PTHREAD_COND_INITIALIZER;
+int countIn;
+int canComeIn;
 
 void wait_barrier(int n) {
     /* TODO: WAIT ON BARRIER UPON EVERY ITERATION */
+    
+    pthread_mutex_lock(&mx);
+    while (canComeIn == 0) {
+        pthread_cond_wait(&condOut, &mx);
+    }
+    countIn++;
+    if (countIn < n) {
+        pthread_cond_wait(&condIn, &mx);
+    } else {
+        canComeIn = 0;
+        pthread_cond_broadcast(&condIn);
+    }
+    pthread_mutex_unlock(&mx);
+
+    pthread_mutex_lock(&mx);
+    countIn--;
+    if (countIn == 0) {
+        canComeIn = 1;
+        pthread_cond_broadcast(&condOut);
+    }
+    pthread_mutex_unlock(&mx);
 }
 
 
@@ -23,6 +49,7 @@ void* thread_func(void* arg) {
     int i;
     printf ("start barriers\n");
     for (i = 0; i < NB_LOOPS; i++) {
+        printf ("before barrier %d\n", i);
         wait_barrier (NB_THREADS);
         printf ("after barrier %d\n", i);
     }
@@ -35,8 +62,8 @@ int main(int argc, char** argv) {
     int i;
     pthread_t tid[NB_THREADS];
     
-    countup = 0;
-    countdown = 0;
+    countIn = 0;
+    canComeIn = 1;
     
     for (i = 0; i < NB_THREADS; i++)
         pthread_create((tid+i), 0, thread_func, 0);
