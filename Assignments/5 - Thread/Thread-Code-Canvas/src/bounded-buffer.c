e63f1ff9264c2830c0a7a8cb6547fbd2b54fb44f
Gavin Jin <jj2915@nyu.edu>
2022-11-23 19:01:20 +0800

    finish assigment 4

--- Assignments/5 - Thread/Thread-Code-Canvas/src/bounded-buffer.c
+++ Assignments/5 - Thread/Thread-Code-Canvas/src/bounded-buffer.c
@@ -5,53 +5,83 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#define NP 4
-#define NC 3
-#define MAX_CAP 2
-#define NVAL 5
+#define NP 3
+#define NC 2
+#define MAX_CAP 3
+#define NVAL 10
 
-int in_production;
-int consumed;
-int next_i_p;
+int inside, consumed;
 int stack[MAX_CAP];
-pthread_mutex_t m_p = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_p = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_c = PTHREAD_COND_INITIALIZER;
+
+void terminate_if_possible() {
+    if (consumed == NVAL) {
+        inside = -1;
+        pthread_cond_broadcast(&cond_p);
+        pthread_cond_broadcast(&cond_c);
+        pthread_mutex_unlock(&m);
+        pthread_exit((void*) 0);
+    }
+}
 
 void * consumer_thread(void * args) {
-    pthread_exit((void*) 0);
+    while (1) {
+        pthread_mutex_lock(&m);
+        
+        // If empty, wait
+        while (inside == 0) {
+            pthread_cond_wait(&cond_p, &m);
+        }
+
+        terminate_if_possible();
+        
+        // start consuming
+        inside--;
+        printf("Consumer %ld consumes %d\n", (long) pthread_self(), stack[inside]);
+        stack[inside] = 0;
+        consumed++;
+
+        // Release resource for producer
+        pthread_cond_signal(&cond_c);
+        
+        pthread_mutex_unlock(&m);
+    }
+    
 }
 
 
 void * producer_thread(void * args) {
-    int local_next_i;
-
     while (1) {
-        // only allow MAX_CAP producer to produce
-        pthread_mutex_lock(&m_p);
-        if (in_production < MAX_CAP) {
-            pthread_cond_wait(&cond_p, m_p);
-        } else {
-            in_production++;
-            local_next_i = next_i_p;
-            next_i_p = (next_i_p + 1) % MAX_CAP;
+        pthread_mutex_lock(&m);
+
+        // If full, wait
+        while (inside == MAX_CAP) {
+            pthread_cond_wait(&cond_c, &m);
         }
-        pthread_mutex_unlock(&m_p);
 
-        // produce
+        terminate_if_possible();
 
+        // start producing
+        stack[inside] = rand() % 100 + 1;
+        inside++;
+        
+        // Release resource for consumer
+        pthread_cond_signal(&cond_p);
+
+        pthread_mutex_unlock(&m);
     }
     
-
-    pthread_exit((void*) 0);
 }
 
 int main() {
     int i, j, status;
     pthread_t tid_p[NP], tid_c[NC];
 
-    in_production = 0;
+    srand(getpid());
+    inside = 0;
     consumed = 0;
-    next_i_p = 0;
 
     for (i = 0; i < NP; i++) {
         if (pthread_create(&(tid_p[i]), NULL, producer_thread, NULL) != 0) {
@@ -65,8 +95,7 @@ int main() {
             perror("pthread_create");
             exit(1);
         }
-    }
-
+    } 
     
     for (j = 0; j < NP; j++) {
         if (pthread_join(tid_p[j], (void**) &status) != 0) {
